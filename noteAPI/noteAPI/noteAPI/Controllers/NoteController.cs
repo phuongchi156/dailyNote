399da27b7768c1610ee4e077a90c40c74fce2349
phuongchi156 <58780929+phuongchi156@users.noreply.github.com>
2023-10-24 14:26:53 +0700

    Add files via upload

---
+++ noteAPI/noteAPI/noteAPI/Controllers/NoteController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace noteAPI.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoteController : ControllerBase
+    {
+        public NoteController() { }
+
+        [HttpGet("")]
+        public String GetAllNote()
+        {
+            return "xin chào";
+        }
+    }
+}
