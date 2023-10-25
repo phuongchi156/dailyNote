c7bd16ec056320afa9764c77145f1318c6d9d365
phuongchi156 <58780929+phuongchi156@users.noreply.github.com>
2023-10-25 16:52:52 +0700

    upload angular project 2

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { NoteComponent } from './note/note.component';
+
+const routes: Routes = [
+  { path: 'note', component: NoteComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
